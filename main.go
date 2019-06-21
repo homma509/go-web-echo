6aa08c4891a78cddeeee2122a02717b5bce6b1e1
kokihonma <kouki.honma@gmail.com>
2019-06-22 08:33:46 +0900

    Add implementation of JSON request and validation

--- main.go
+++ main.go
@@ -1,20 +1,80 @@
 package main
 
 import (
+	"errors"
+	"log"
 	"net/http"
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/locales/ja_JP"
+	ut "github.com/go-playground/universal-translator"
+	"gopkg.in/go-playground/validator.v9"
+	ja "gopkg.in/go-playground/validator.v9/translations/ja"
 
 	"github.com/labstack/echo"
 )
+
+// Validator is implementaion of validation of request values.
+type Validator struct {
+	trans     ut.Translator
+	validator *validator.Validate
+}
+
+// Validate do validation for request value.
+func (v *Validator) Validate(i interface{}) error {
+	err := v.validator.Struct(i)
+	if err == nil {
+		return nil
+	}
+	errs := err.(validator.ValidationErrors)
+	msg := ""
+	for _, v := range errs.Translate(v.trans) {
+		if msg != "" {
+			msg += ", "
+		}
+		msg += v
+	}
+	return errors.New(msg)
+}
+
+// Error indicate response error
+type Error struct {
+	Error string `json:"error"`
+}
 
 // Comment is a struct to hold unit of request
 type Comment struct {
 	ID   int64  `json:"id"`
 	Name string `json:"name" form:"name"`
-	Text string `json:"text" form:"text"`
+	Text string `json:"text" form:"text" validate:"required,max=20"`
 }
 
 func main() {
 	e := echo.New()
+
+	japanese := ja_JP.New()
+	uni := ut.New(japanese, japanese)
+	trans, _ := uni.GetTranslator("ja")
+	validate := validator.New()
+	err := ja.RegisterDefaultTranslations(validate, trans)
+	if err != nil {
+		log.Fatal(err)
+	}
+	validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+		name := strings.SplitN(fld.Tag.Get("json"), ",", 2)[0]
+		switch name {
+		case "name":
+			return "お名前"
+		case "text":
+			return "コメント"
+		case "-":
+			return ""
+		}
+		return name
+	})
+	e.Validator = &Validator{validator: validate, trans: trans}
+
 	// e.GET("/", func(c echo.Context) error {
 	// 	name := c.QueryParam("name")
 	// 	return c.String(http.StatusOK, "Hello " + name)
@@ -26,9 +86,14 @@ func main() {
 	e.POST("api/comments", func(c echo.Context) error {
 		var comment Comment
 		if err := c.Bind(&comment); err != nil {
+			c.Logger().Error("Bind: ", err)
 			return c.String(http.StatusBadRequest, "Bind: "+err.Error())
 		}
-		return c.String(http.StatusOK, "name " + comment.Name)
+		if err := c.Validate(&comment); err != nil {
+			c.Logger().Error("Validate: ", err)
+			return c.JSON(http.StatusBadRequest, &Error{Error: err.Error()})
+		}
+		return c.JSON(http.StatusCreated, "OK!")
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
