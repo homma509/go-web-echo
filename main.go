74677ea7aa159c31d1be4f95f8e21297c18d67b2
kokihonma <kouki.honma@gmail.com>
2019-06-22 06:34:35 +0900

    Add Comment struct to hold unit of request

--- main.go
+++ main.go
@@ -6,10 +6,29 @@ import (
 	"github.com/labstack/echo"
 )
 
+// Comment is a struct to hold unit of request
+type Comment struct {
+	ID   int64  `json:"id"`
+	Name string `json:"name" form:"name"`
+	Text string `json:"text" form:"text"`
+}
+
 func main() {
 	e := echo.New()
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello")
+	// e.GET("/", func(c echo.Context) error {
+	// 	name := c.QueryParam("name")
+	// 	return c.String(http.StatusOK, "Hello " + name)
+	// })
+	// e.GET("/:name", func(c echo.Context) error {
+	// 	name := c.Param("name")
+	// 	return c.String(http.StatusOK, "Hello " + name)
+	// })
+	e.POST("api/comments", func(c echo.Context) error {
+		var comment Comment
+		if err := c.Bind(&comment); err != nil {
+			return c.String(http.StatusBadRequest, "Bind: "+err.Error())
+		}
+		return c.String(http.StatusOK, "name " + comment.Name)
 	})
 	e.Logger.Fatal(e.Start(":8080"))
 }
